8e363f6761b604cbb3f64781d557fa5b9633414f
Pikaman64Turbo <jstb18@gmail.com>
2016-09-15 19:54:57 -0400

    first commit

---
+++ Assets/C#/Playerwavelol.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playerwavelol : MonoBehaviour {
+
+	// Use this for initialization
+
+		public float speed;
+
+		private Rigidbody rb;
+
+		void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
