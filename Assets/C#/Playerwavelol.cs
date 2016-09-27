f48be2c253e864c46dbdd0586d8901a207157426
Pikaman64Turbo <jstb18@gmail.com>
2016-09-27 19:55:54 -0400

    I think I have an error with a text message, I need help.

--- Assets/C#/Playerwavelol.cs
+++ Assets/C#/Playerwavelol.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Playerwavelol : MonoBehaviour {
@@ -6,12 +7,18 @@ public class Playerwavelol : MonoBehaviour {
 	// Use this for initialization
 
 		public float speed;
+		public Text levelCount;
+		public Text winText;
 
 		private Rigidbody rb;
+		private int count;
 
 		void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetlevelCount ();
+		//winText.text = "";
 
 	}
 	
@@ -23,5 +30,24 @@ public class Playerwavelol : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Collectable1")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetlevelCount ();
+		}
+	}
+
+	void SetlevelCount ()
+	{
+		levelCount.text = "Count: " + count.ToString ();
+		Debug.Log(levelCount.text);
+		if (count >= 1)
+		{
+		//	winText.text = "You Win!";
+		}
 	}
 }
